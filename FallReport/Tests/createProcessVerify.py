7dea6d156911f788c9643b58f70163ee2bf73e9d
bozer22 <beyza.ozer@aspireci.com>
2022-08-08 23:54:56 -0600

    incident report created/processed/verified

--- FallReport/Tests/createProcessVerify.py
+++ FallReport/Tests/createProcessVerify.py
@@ -7,9 +7,7 @@ import time
 from random import randrange
 from random import seed
 from selenium.webdriver.common.keys import Keys
-from datetime import datetime
-from datetime import date
-
+from _datetime import datetime, date
 
 executable_path = "C:/Users/beyza.ozer/Downloads/chromedriver/chromedriver.exe"
 webPage = "https://www.rtasks.net/Users/Login"
@@ -43,6 +41,7 @@ class LoginTestRtasks(unittest.TestCase):
         self.driver.find_element_by_xpath("//select[@id='campus']").click()
         self.driver.find_element_by_xpath("//option[@value='1']").click()
         self.driver.find_element_by_xpath("//a[@class='ui huge fluid primary button']").click()
+
 # Click resident button
         self.driver.find_element_by_xpath("//a[@ui-sref='Residents']").click()
         time.sleep(5)
@@ -58,6 +57,9 @@ class LoginTestRtasks(unittest.TestCase):
         print(num)
         nam = names[num]
 
+        nem = nam[:9]
+        print(nem)
+
         # Select 'test campus'
         self.driver.find_element_by_xpath("//span[@ng-bind='session.user.FullName']").click()
         time.sleep(5)
@@ -71,7 +73,7 @@ class LoginTestRtasks(unittest.TestCase):
         # report incident
         self.driver.find_element_by_xpath("//a[@data-ui-sref='ReportIncident']").click()
         # enter date,time,category,type,resident
-        self.driver.find_element_by_id("date").send_keys("07/14/2022")
+        self.driver.find_element_by_id("date").send_keys("08/08/2022")
         self.driver.find_element_by_id("time").send_keys("8:00 PM")
         time.sleep(3)
         self.driver.find_element_by_name("category").send_keys("Incident-Resident")
@@ -200,6 +202,14 @@ class LoginTestRtasks(unittest.TestCase):
         self.driver.find_element_by_xpath("//a[@href='/events/falls']").click()
         time.sleep(45)
 
+        # Click 'Open Fall Events' dropdown
+        self.driver.find_element_by_xpath("//select[@class='svelte-1jf0zp4']").click()
+        time.sleep(3)
+
+        # Select patient to process
+        self.driver.find_element_by_xpath("//option[@value='[object Object]'][1]").click()
+
+
         # Experienced Injury? No
         self.driver.find_element_by_xpath("(//input[@value='no'])[1]").click()
         time.sleep(1)
@@ -209,7 +219,7 @@ class LoginTestRtasks(unittest.TestCase):
         time.sleep(1)
 
 
-        #EMS was Involved? No
+        # EMS was Involved? No
         self.driver.find_element_by_xpath("(//input[@value='no'])[3]").click()
         time.sleep(1)
 
@@ -315,10 +325,7 @@ class LoginTestRtasks(unittest.TestCase):
         self.driver.get(webPage)
         self.driver.implicitly_wait(5)
         self.driver.maximize_window()
-        # Login to 'rtasks.net'
-        # self.driver.find_element_by_id("Username").send_keys(USER_NAME)
-        # self.driver.find_element_by_id("Password").send_keys(PASSWORD)
-        # self.driver.find_element_by_xpath("//button[@type = 'submit']").click()
+
         # Select 'test campus'
         self.driver.find_element_by_xpath("//span[@ng-bind='session.user.FullName']").click()
         time.sleep(5)
@@ -332,23 +339,8 @@ class LoginTestRtasks(unittest.TestCase):
         time.sleep(5)
 
 
-
-        ######
-        # names1 = self.driver.find_elements_by_class_name("ng-binding")
-        # names2 = []
-
-        # for i in names1:
-        #     names2.append(i.text)
-        # namess = names2[20:-1]
-        # names = namess[::4]
-        # num = randrange(3)
-        # print(num)
-        # nam = names[num]
-        # ####
-
-
-        nem = nam[:9]
-        print(nem)
+        # nem = nam[:9]
+        # print(nem)
 
 
         if 20 < num <= 40:
@@ -357,15 +349,6 @@ class LoginTestRtasks(unittest.TestCase):
             time.sleep(3)
             self.driver.execute_script("window.scrollTo(0, window.scrollY + 1200)")
 
-
-
-        # self.driver.execute_script("window.scrollTo(0, window.scrollY + 200)")
-
-        #
-        #
-        #     else
-        #     self.driver.execute_script("window.scrollTo(0, window.scrollY + 200)")
-        #     time.sleep(10)
 
         time.sleep(10)
         self.driver.find_element_by_xpath(f"//strong[contains(text(),  '{nem}')]").click()
@@ -402,45 +385,5 @@ class LoginTestRtasks(unittest.TestCase):
         else:
             print('Test failed because the last report was NOT today')
 
-
-
-
-        '''self.driver.find_element(by=By.XPATH, value="//a[@ui-sref='Residents']").click()
-        time.sleep(5)
-
-        names1 = self.driver.find_elements_by_class_name("ng-binding")
-        names2 = []
-
-        for i in names1:
-            names2.append(i.text)
-        namess = names2[20:-1]
-        names = namess[::4]
-        num = randrange(20)
-        print(num)
-        nam = names[num]
-        nem = nam[:9]
-        print(nem)
-
-        # if (num > 20)
-        #     else
-        #     self.driver.execute_script("window.scrollTo(0, window.scrollY + 200)")
-        #     time.sleep(10)
-
-
-        time.sleep(10)
-        self.driver.find_element(by=By.XPATH, value=f"//strong[contains(text(),  '{nem}')]").click()
-        time.sleep(10)
-        # Scroll down
-        self.driver.execute_script("window.scrollTo(0, window.scrollY + 200)")
-        time.sleep(10)
-
-        # Click Notes
-        self.driver.find_element(by=By.XPATH, value="(//div[@class='content ng-scope'])[7]").click()
-        time.sleep(10)
-        self.driver.execute_script("window.scrollTo(0, window.scrollY + 200)")
-        # verify to see electronically by Beyza Ozer and time stamp
-        time.sleep(10)
-        var = self.driver.find_element_by_xpath("(//tr[@ng-repeat])").text()
-        print(var)'''
 
 
